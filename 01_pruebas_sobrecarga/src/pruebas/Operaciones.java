8203d9aac6fab8106537f8d2db6311af5470493d
tarde <tarde@A13E21>
2023-10-06 19:35:50 +0200

    commit proyectos 1 y 2

---
+++ 01_pruebas_sobrecarga/src/pruebas/Operaciones.java
@@ -0,0 +1,36 @@
+package pruebas;
+
+public class Operaciones {
+	//Constructor por defecto. Lo crea Java si la clase no tiene constructores explícitos
+	/*public Operaciones() {
+		
+	}*/
+	public int sum(String s, int ... numeros ) {
+		int suma=0;
+		for(int num:numeros) {
+			suma+=num;
+		}
+		return suma;
+	}
+	//si se llama con un int, el orden de preferencia es este:
+	public int multi(int s) {
+		return 10;
+	}
+	public int multi(long k) {
+		return 100;
+	}
+	public int multi(Integer n) {
+		return 40;
+	}
+	public int multi(int...datos) {
+		return 30;
+	}
+	
+	//sobrecarga no válida
+	public int div(int ...is nums) {
+		return 200;
+	}
+	public int div(int[] valores) {
+		return 300;
+	}
+}
---
+++ 02_ejemplo_herencia/src/principal/Punto3D.java
@@ -0,0 +1,22 @@
+package principal;
+
+public class Punto3D extends Punto {
+	private int z;
+	public Punto3D(int z) {
+		this.z=z;
+	}
+	public Punto3D(int x, int y, int z) {
+		super(x,y);
+		this.z=z;
+	}
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
+	public void dibujar() {
+		System.out.println("Coordenadas: "+getX()+","+getY()+","+getZ());
+	}
+	
+}
